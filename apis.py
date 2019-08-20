ce91b28c75e76a6e515b50e07aa4fd15d9387937
Olivia Jain <oliviajain@Olivias-MacBook-Pro-2.local>
2019-08-19 17:31:43 -0700

    refactored all files to follow PEP convention

--- apis.py
+++ apis.py
@@ -2,33 +2,33 @@ import sys
 import requests
 import time
 import json
-# If you are using a Jupyter notebook, uncomment the following line.
-# %matplotlib inline
 import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
 from PIL import Image
 from io import BytesIO
-
+from matplotlib.patches import Polygon
 from grammarbot import GrammarBotClient
 from collections import Counter
 
-def convertToText(image_path, overlayImageWithExtractedText = True):
-    # Replace <Subscription Key> with your valid subscription key.
+def convert_to_text(image_path, overlay_image = True):
+
     subscription_key = "TODO: FILL IN SUBSCRIPTION KEY"
     assert subscription_key
 
-    vision_base_url = "https://westus.api.cognitive.microsoft.com/vision/v2.0/"
-
-    text_recognition_url = vision_base_url + "read/core/asyncBatchAnalyze"
     mode = "Handwritten"
+    vision_base_url = "https://westus.api.cognitive.microsoft.com/vision/v2.0/"
+    text_recognition_url = vision_base_url + "read/core/asyncBatchAnalyze"
     analyze_url = vision_base_url + "recognizeText?mode=" + mode
 
     # Read the image into a byte array
     image_data = open(image_path, "rb").read()
-    headers = {'Ocp-Apim-Subscription-Key': subscription_key,
-               'Content-Type': 'application/octet-stream'}
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        'Content-Type': 'application/octet-stream'
+    }
     response = requests.post(
-        analyze_url, headers=headers, data=image_data)
+        analyze_url, 
+        headers=headers, 
+        data=image_data)
     response.raise_for_status()
 
     output_name = "static/output.jpg"
@@ -36,7 +36,8 @@ def convertToText(image_path, overlayImageWithExtractedText = True):
     poll = True
     while (poll):
         response_final = requests.get(
-            response.headers["Operation-Location"], headers=headers)
+            response.headers["Operation-Location"], 
+            headers=headers)
         analysis = response_final.json()
         time.sleep(1)
         if ("recognitionResult" in analysis):
@@ -44,24 +45,38 @@ def convertToText(image_path, overlayImageWithExtractedText = True):
         if ("status" in analysis and analysis['status'] == 'Failed'):
             poll = False
 
-    if overlayImageWithExtractedText == True:
+    if overlay_image == True:
         polygons = []
         if ("recognitionResult" in analysis):
             # Extract the recognized text, with bounding boxes.
             polygons = [(line["boundingBox"], line["text"])
                         for line in analysis["recognitionResult"]["lines"]]
 
-        #Display the image and overlay it with the extracted text.
+        # Display the image and overlay it with the extracted text.
         plt.figure(figsize=(15, 15))
         image = Image.open(image_path)
         ax = plt.imshow(image)
+        
         for polygon in polygons:
             vertices = [(polygon[0][i], polygon[0][i+1])
                         for i in range(0, len(polygon[0]), 2)]
             text = polygon[1]
-            patch = Polygon(vertices, closed=True, fill=False, linewidth=2, color='y')
+            
+            patch = Polygon(
+                        vertices, 
+                        closed=True, 
+                        fill=False, 
+                        linewidth=2, 
+                        color='y')
             ax.axes.add_patch(patch)
-            plt.text(vertices[0][0], vertices[0][1], text, fontsize=20, va="top")
+            
+            plt.text(
+                vertices[0][0], 
+                vertices[0][1], 
+                text, 
+                fontsize=20, 
+                va="top")
+        
         plt.show()
         image.save(output_name)
 
@@ -73,46 +88,51 @@ def convertToText(image_path, overlayImageWithExtractedText = True):
     return lines_of_text
 
 
-def grammarAnalysis(linesOfText):
+def grammar_analysis(lines_of_text):
     client = GrammarBotClient(api_key='TODO: FILL IN API KEY')
-    text = ''
-    for txt in linesOfText: 
-        text += (str(txt) + '/')
+    all_text = ''
+    for txt in lines_of_text: 
+        all_text += (str(txt) + '/')
 
     # check the text, returns GrammarBotApiResponse object
-    res = client.check(text) 
+    errors = client.check(text) 
 
     # get error counts
     metadata = {}
     error_types = []
-    for err in res.matches: 
-        start_index = err.replacement_offset 
-        end_index = start_index + err.replacement_length
-        mistake = text[start_index:end_index]
+    for err in errors.matches: 
+        start = err.replacement_offset 
+        end = start + err.replacement_length
+        mistake = allText[start:end]
+        
         # format suggestions
         suggestions = ''
-        for sugg in err.replacements:
-            suggestions += sugg + ', '
-        metadata[mistake] = suggestions[:-2]
+        for suggestion in err.replacements:
+            suggestions += suggestion + ', '
+        metadata[mistake] = suggestions[:-2] # take off last comma
         error_types.append(err.category)
 
+    # get line numbers for help
     line_numbers = []
     for mistake in metadata.keys(): 
         for i, s in enumerate(linesOfText):
             if mistake in s:
                 line_numbers.append(i)
 
-    error_counts = dict(Counter(error_types))
-    error_counts_list = [(err, count) for err, count in error_counts.items()]
-    total_errors = sum(error_counts.values())
+
+    type_counts = dict(Counter(errorTypes))
+    type_counts_list = type_counts.items()
+    total_errors = sum(types_counts.values())
 
     result_string = json.dumps(res.raw_json, indent=4)
 
     mistake_info = []
     for i, mistake in enumerate(metadata.keys()):
-        mistake_info.append((line_numbers[i], mistake, metadata[mistake]))
+        mistake_info.append((
+            line_numbers[i], 
+            mistake, 
+            metadata[mistake]))
     
-
-    flag = (total_errors / len(linesOfText) > 1)
-    return total_errors, error_counts_list, mistake_info, flag
+    flag = (total_errors / len(lines_of_text) > 1)
+    return total_errors, type_counts_list, mistake_info, flag
     
--- routes.py
+++ routes.py
@@ -1,19 +1,24 @@
 from flask import Flask, request, render_template
-from apis import convertToText, grammarAnalysis
+from apis import convert_to_text, grammar_analysis
 
 app = Flask(__name__)
 
 @app.route('/')
-def my_form():
-    return render_template('my-form.html')
+def form():
+    return render_template('form.html')
 
 @app.route('/', methods=['POST'])
-def my_form_post():
+def form_post():
     path = request.form['pic']
     processed_path = path.lower()
-    textualSubmission = convertToText(processed_path)
-    total_errors, error_counts, mistake_info, flag = grammarAnalysis(textualSubmission)
+    textual_submission = convert_to_text(processed_path)
+    total_errors, error_counts, mistake_info, flag = grammar_analysis(textual_submission)
     return display_report(total_errors, error_counts, mistake_info, flag)
 
 def display_report(total_errors, error_counts, mistake_info, flag):
-	return render_template('report.html', total_errors=total_errors, error_counts=error_counts, mistake_info=mistake_info, flag=flag)
+	return render_template(
+		'report.html',
+		total_errors=total_errors,
+		error_counts=error_counts, 
+		mistake_info=mistake_info, 
+		flag=flag)
